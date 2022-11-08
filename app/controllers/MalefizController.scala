015d3d3213ab52c537cf13a15210c8cbd22ef9d4
sehirsig <sebastianhirsig@bluewin.ch>
2022-11-08 10:10:29 +0100

    Deleted Test files

--- app/controllers/MalefizController.scala
+++ app/controllers/MalefizController.scala
@@ -95,14 +95,6 @@ class MalefizController @Inject()(cc: ControllerComponents) extends AbstractCont
     addText
   }
 
-  def bootstrapTest = Action {
-    Ok(views.html.bootstraptest())
-  }
-
-  def navigationTest = Action {
-    Ok(views.html.navigationtest())
-  }
-
   val gameData = new gameData()
 
   def getStatusID(): Int = {
